028f19d3b0d28ada81fc1de3f30511d3980a4e0a
Ahmed Soliman <asoliman@cloud9ers.com>
2015-09-13 13:45:35 +0200

    fixing bootup

--- bootup.sh
+++ bootup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 set -x
-source sfs2x.sh $@
+JRE_FOLDER="../jre/bin"
+JAVA_CMD="java"
+if [ -d "$JRE_FOLDER" ]; then
+    JAVA_CMD="../jre/bin/java"
+    fi
+
+    CPATH="./:lib/*:lib/jetty/lib/*:extensions/__lib__/*"
+    ${JAVA_CMD} -cp ${CPATH} -Dfile.encoding=UTF-8 com.smartfoxserver.v2.Main $@
